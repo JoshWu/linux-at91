a63169895752d4d7972c0acf64c4926693682d30
Josh Wu <josh.wu@atmel.com>
2015-06-17 18:10:01 +0800

    media: atmel-isi: support RGB565 output when sensor output YUV formats
    This patch enable Atmel ISI preview path to convert the YUV to RGB format.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>

--- drivers/media/platform/soc_camera/atmel-isi.c
+++ drivers/media/platform/soc_camera/atmel-isi.c
@@ -230,11 +230,15 @@ static bool is_supported(struct soc_camera_device *icd,
 	bool ret = true;
 
 	switch (host_fmt->fourcc) {
+	/* YUV */
 	case V4L2_PIX_FMT_YUYV:
 	case V4L2_PIX_FMT_UYVY:
 	case V4L2_PIX_FMT_YVYU:
 	case V4L2_PIX_FMT_VYUY:
+	/* GREY */
 	case V4L2_PIX_FMT_GREY:
+	/* RGB */
+	case V4L2_PIX_FMT_RGB565:
 		break;
 	default:
 		dev_err(icd->parent, "not support output memory v4l2 format: %c%c%c%c\n",
@@ -243,6 +247,12 @@ static bool is_supported(struct soc_camera_device *icd,
 	}
 
 	return ret;
+}
+
+static bool is_output_rgb(const struct soc_mbus_pixelfmt *host_fmt)
+{
+       return host_fmt->fourcc == V4L2_PIX_FMT_RGB565 ||
+	       host_fmt->fourcc == V4L2_PIX_FMT_RGB32;
 }
 
 static irqreturn_t atmel_isi_handle_streaming(struct atmel_isi *isi)
@@ -502,6 +512,8 @@ static int start_streaming(struct vb2_queue *vq, unsigned int count)
 	struct atmel_isi *isi = ici->priv;
 	int ret;
 
+	isi->enable_preview_path = is_output_rgb(icd->current_fmt->host_fmt);
+
 	pm_runtime_get_sync(ici->v4l2_dev.dev);
 
 	/* Reset ISI */
@@ -725,6 +737,14 @@ static const struct soc_mbus_pixelfmt isi_camera_formats[] = {
 		.order			= SOC_MBUS_ORDER_LE,
 		.layout			= SOC_MBUS_LAYOUT_PACKED,
 	},
+	{
+		.fourcc			= V4L2_PIX_FMT_RGB565,
+		.name			= "RGB565",
+		.bits_per_sample	= 8,
+		.packing		= SOC_MBUS_PACKING_2X8_PADHI,
+		.order			= SOC_MBUS_ORDER_LE,
+		.layout			= SOC_MBUS_LAYOUT_PACKED,
+	},
 };
 
 /* This will be corrected as we get more formats */
@@ -781,7 +801,7 @@ static int isi_camera_get_formats(struct soc_camera_device *icd,
 				  struct soc_camera_format_xlate *xlate)
 {
 	struct v4l2_subdev *sd = soc_camera_to_subdev(icd);
-	int formats = 0, ret;
+	int formats = 0, ret, i, n;
 	/* sensor format */
 	struct v4l2_subdev_mbus_code_enum code = {
 		.which = V4L2_SUBDEV_FORMAT_ACTIVE,
@@ -815,13 +835,16 @@ static int isi_camera_get_formats(struct soc_camera_device *icd,
 	case MEDIA_BUS_FMT_VYUY8_2X8:
 	case MEDIA_BUS_FMT_YUYV8_2X8:
 	case MEDIA_BUS_FMT_YVYU8_2X8:
-		formats++;
-		if (xlate) {
-			xlate->host_fmt	= &isi_camera_formats[0];
-			xlate->code	= code.code;
-			dev_dbg(icd->parent, "Providing format %s (%s)\n",
-				xlate->host_fmt->name, mbus_fmt_string(xlate->code));
-			xlate++;
+		n = ARRAY_SIZE(isi_camera_formats);
+		formats += n;
+		for (i = 0; i < n; i++) {
+			if (xlate) {
+				xlate->host_fmt	= &isi_camera_formats[i];
+				xlate->code	= code.code;
+				dev_dbg(icd->parent, "Providing format %s (%s)\n",
+					xlate->host_fmt->name, mbus_fmt_string(xlate->code));
+				xlate++;
+			}
 		}
 		break;
 	default:
