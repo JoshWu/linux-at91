19e1c356c129ceb5371231ddfbff43c540a6460e
Josh Wu <josh.wu@atmel.com>
2015-09-29 16:27:42 +0800

    media: atmel-isi: use start_dma()
    NOTE: sometimes only can capture 31 frames
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>

--- drivers/media/platform/soc_camera/atmel-isi.c
+++ drivers/media/platform/soc_camera/atmel-isi.c
@@ -216,6 +216,7 @@ static bool is_output_rgb(const struct soc_mbus_pixelfmt *host_fmt)
 			host_fmt->fourcc == V4L2_PIX_FMT_RGB32;
 }
 
+static void start_dma(struct atmel_isi *isi, struct frame_buffer *buffer);
 static irqreturn_t atmel_isi_handle_streaming(struct atmel_isi *isi)
 {
 	if (isi->active) {
@@ -234,19 +235,7 @@ static irqreturn_t atmel_isi_handle_streaming(struct atmel_isi *isi)
 		/* start next dma frame. */
 		isi->active = list_entry(isi->video_buffer_list.next,
 					struct frame_buffer, list);
-		if (!isi->enable_preview_path) {
-			isi_writel(isi, ISI_DMA_C_DSCR,
-				(u32)isi->active->p_dma_desc->fbd_phys);
-			isi_writel(isi, ISI_DMA_C_CTRL,
-				ISI_DMA_CTRL_FETCH | ISI_DMA_CTRL_DONE);
-			isi_writel(isi, ISI_DMA_CHER, ISI_DMA_CHSR_C_CH);
-		} else {
-			isi_writel(isi, ISI_DMA_P_DSCR,
-				(u32)isi->active->p_dma_desc->fbd_phys);
-			isi_writel(isi, ISI_DMA_P_CTRL,
-				ISI_DMA_CTRL_FETCH | ISI_DMA_CTRL_DONE);
-			isi_writel(isi, ISI_DMA_CHER, ISI_DMA_CHSR_P_CH);
-		}
+		start_dma(isi, isi->active);
 	}
 	return IRQ_HANDLED;
 }
@@ -417,11 +406,6 @@ static void start_dma(struct atmel_isi *isi, struct frame_buffer *buffer)
 
 	/* Check if already in a frame */
 	if (!isi->enable_preview_path) {
-		if (isi_readl(isi, ISI_STATUS) & ISI_CTRL_CDC) {
-			dev_err(isi->soc_host.icd->parent, "Already in frame handling.\n");
-			return;
-		}
-
 		isi_writel(isi, ISI_DMA_C_DSCR,
 				(u32)buffer->p_dma_desc->fbd_phys);
 		isi_writel(isi, ISI_DMA_C_CTRL,
